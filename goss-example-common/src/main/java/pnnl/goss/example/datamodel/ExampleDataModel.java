f73ccbc414d0ff4ce501c101deec9aff23c238c2
poorvasharma <poorva1209@gmail.com>
2015-01-15 16:00:19 -0800

    initial commit

---
+++ goss-example-common/src/main/java/pnnl/goss/example/datamodel/ExampleDataModel.java
@@ -0,0 +1,5 @@
+package pnnl.goss.example.datamodel;
+
+public class ExampleDataModel {
+
+}
---
+++ goss-example-common/src/main/java/pnnl/goss/example/request/ExampleRequest.java
@@ -0,0 +1,9 @@
+package pnnl.goss.example.request;
+
+import pnnl.goss.core.Request;
+
+public class ExampleRequest extends Request{
+	
+	
+
+}
